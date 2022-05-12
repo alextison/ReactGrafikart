0a6aa57d401df11c663aa888489228a0e1e2d3f9
Torfa <Alexandre.Tison@etu.univ-littoral.fr>
2022-05-12 17:39:17 +0200

    Mac to GitHub

---
+++ app.js
@@ -0,0 +1,15 @@
+let n = 0
+
+function render(){
+    const title = <h1>Hello World
+        <span>{n}</span>
+    </h1>
+    ReactDOM.render(title, document.querySelector('#app'))
+}
+
+render()
+
+window.setInterval(() => {
+    n++
+    render()
+}, 1000)
